5fcbbfee2b61d69dafc5264ef12dca4a8203b533
unknown <vardalagoy@gmail.com>
2019-03-09 15:34:35 -0800

    Adding options for address, color, and time with defaults

--- custom_timed_event.py
+++ custom_timed_event.py
@@ -17,23 +17,36 @@ SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 def main():
     postEvent(buildEvent())
 
-#TODO - custom colors :D 
+
+#TODO - option/flag for more input
+
+##Defaults - Can I have an option to read in previous event and offer that as default? 
+#Summary: Name 
+#Location: Blank/Current
+#Color: Blank/Green 
+#Description: Blank
+#Length: 30 
+
+
 def buildEvent():
     current_time = datetime.datetime.now()
-    end_time = current_time + datetime.timedelta(minutes=30)
-    event_summary = input("Please enter the name of your time block: ")
-    event_description = input("Would you like to enter a description for your time block? Press Enter to skip: ")
+    event_summary = input("Please enter the name of your time block: ") #TODO - Can I make the following inputs optional? Let the user skip all and set to defaults if 
+    event_location = input("Would you like to enter an address? Press Enter to skip: ") or "17348 SW Lawton Beaverton 97003"
+    event_color = input("What color is your event? Choose a number 1-11 or press Enter to skip: ") or 3
+    event_description = input("Would you like to enter a description for your time block? Press Enter to skip: ") 
+    event_length = input("How long is your event (in minutes)? Press Enter for default 30 minutes. ") or 30
+    end_time = current_time + datetime.timedelta(minutes=int(event_length))
     full_event_info = {
         'summary': event_summary,
-        'location': '17348 SW Lawton St., Beaverton, OR 97003', #TODO - replace with current location
-        'colorId': '5',
+        'location': event_location, 
+        'colorId': event_color,
         'description': event_description,
         'start': {
             'dateTime': current_time.isoformat(),
             'timeZone': 'America/Los_Angeles'
         },
         'end': {
-            'dateTime': end_time.isoformat(), #TODO - custom end time 
+            'dateTime': end_time.isoformat(),  
             'timeZone': 'America/Los_Angeles'
         },
     }
